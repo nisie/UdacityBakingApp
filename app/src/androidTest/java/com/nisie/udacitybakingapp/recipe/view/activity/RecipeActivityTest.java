b1c71d05d3d008f77dbffb85fff3ac7988a4daef
nisie <nathania.sutedja@tokopedia.com>
2017-10-01 10:27:12 +0700

    Add espresso test

---
+++ app/src/androidTest/java/com/nisie/udacitybakingapp/recipe/view/activity/RecipeActivityTest.java
@@ -0,0 +1,91 @@
+package com.nisie.udacitybakingapp.recipe.view.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.nisie.udacitybakingapp.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecipeActivityTest {
+
+    @Rule
+    public ActivityTestRule<RecipeActivity> mActivityTestRule = new ActivityTestRule<>(RecipeActivity.class);
+
+    @Test
+    public void recipeActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.recipe_name), withText("Nutella Pie"),
+                        childAtPosition(
+                                allOf(withId(R.id.card_view),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Nutella Pie")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.recipe_name), withText("Brownies"),
+                        childAtPosition(
+                                allOf(withId(R.id.card_view),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Brownies")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.recipe_name), withText("Yellow Cake"),
+                        childAtPosition(
+                                allOf(withId(R.id.card_view),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView3.check(matches(withText("Yellow Cake")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
